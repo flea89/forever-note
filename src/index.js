9f11918efd5e74af5dae9adaf5e193acf6bf6389
Paolo <paolo@potatolondon.com>
2022-09-23 15:09:30 +0100

    Revert "Tidying after myself"
    This reverts commit 409428f2609f417c8a76d14acab7ddca8e33e526.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,47 @@ import { NoteEditor } from "./note-editor";
 import { NoteList } from "./note-list";
 import { Dashboard } from "./dashboard";
 
+const SELECTOR = {
+  authView: "#auth",
+};
+
+console.log("Hello world");
+
+// const authView$ = document.querySelector(SELECTOR.authView);
+
+// if (!authView$) {
+//   throw Error();
+// }
+
+// const showLogin = () => {
+//   authView$.hidden = false;
+//   dashboardView$.hidden = true;
+// };
+
+// const showDashboard = () => {
+//   authView$.hidden = true;
+//   dashboardView$.hidden = false;
+// };
+
+// const loginStatusChangeHandler = (loggedIn, usermetadata) => {
+//   if (loggedIn) {
+//     showDashboard();
+//   } else {
+//     showLogin();
+//   }
+// };
+
+// hydrateAuth(authView$, client, loginStatusChangeHandler);
+
 customElements.define("register-form", RegisterForm);
 customElements.define("app-router", Router);
 customElements.define("notes-app", App);
 customElements.define("notes-dashboard", Dashboard);
 customElements.define("note-editor", NoteEditor);
 customElements.define("note-list", NoteList);
+
+// document
+//   .querySelector("register-form")
+//   ?.addEventListener("registered", (event) => {
+//     console.log(event);
+//   });
